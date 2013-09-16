f5a6670fbe34d6cbbf255c7a1205c3d7d9c8dc74
Alex <alex.orzechowski@hotmail.ca>
2013-09-16 19:09:49 -0400

    cleaned up imports

--- common/trapcraft/common/blocks/BlockMagneticChest.java
+++ common/trapcraft/common/blocks/BlockMagneticChest.java
@@ -2,17 +2,11 @@ package trapcraft.common.blocks;
 
 import java.util.Random;
 
-import trapcraft.TrapcraftMod;
-import trapcraft.api.Properties;
-import trapcraft.common.tileentitys.TileEntityMagneticChest;
-import trapcraft.common.tileentitys.TileEntityTC;
-import need4speed402.mods.barrels.TileEntityBarrel;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
@@ -22,9 +16,11 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Icon;
-import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeDirection;
+import trapcraft.api.Properties;
+import trapcraft.common.tileentitys.TileEntityMagneticChest;
+import trapcraft.common.tileentitys.TileEntityTC;
 
 /**
  * @author ProPercivalalb
