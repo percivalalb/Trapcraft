006bf90efb87f7bc68d91256c62296ba276a75bf
Alex <alex.orzechowski@hotmail.ca>
2013-09-16 19:07:48 -0400

    Update BlockFan.java

--- common/trapcraft/common/blocks/BlockFan.java
+++ common/trapcraft/common/blocks/BlockFan.java
@@ -5,6 +5,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 import trapcraft.TrapcraftMod;
 import trapcraft.api.Properties;
 import trapcraft.common.tileentitys.TileEntityFan;
+import trapcraft.common.tileentitys.TileEntityTC;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
@@ -21,10 +22,12 @@ import net.minecraft.util.Icon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
 
 /**
  * @author ProPercivalalb
- * You may look at this file to gain knowledge of javascript
+ * @author need4speed402
+ * You may look at this file to gain knowledge of java                    
  * but must not copy any features or code directly.
  **/
 public class BlockFan extends BlockContainer {
@@ -76,9 +79,20 @@ public class BlockFan extends BlockContainer {
      * Called when the block is placed in the world.
      */
     @Override
-    public void onBlockPlacedBy(World par1World, int x, int y, int z, EntityLivingBase par5EntityLiving, ItemStack par6ItemStack) {
-        int rotation = determineOrientation(par1World, x, y, z, par5EntityLiving);
-        par1World.setBlockMetadataWithNotify(x, y, z, rotation, 2);
+    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase player, ItemStack stack) {
+        int rotation = 0;
+        
+        if (Math.abs(player.rotationPitch) > 90D / 2D){
+            if (player.rotationPitch > 0){
+                rotation = 1;
+            }else if (player.rotationPitch < 0){
+                rotation = 0;
+            }
+        }else{
+            rotation = Direction.directionToFacing[Direction.rotateOpposite[Math.round(player.rotationYaw / 90) & 3]];
+        }
+		
+        world.setBlockMetadataWithNotify(x, y, z, rotation, 2);
     }
 
     /**
@@ -88,76 +102,38 @@ public class BlockFan extends BlockContainer {
     public void setBlockBoundsForItemRender() {
         setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
     }
-
-    private static int determineOrientation(World world, int i, int j, int k, EntityLivingBase entityplayer) {
-        if (MathHelper.abs((float)entityplayer.posX - (float)i) < 2.0F && MathHelper.abs((float)entityplayer.posZ - (float)k) < 2.0F)
-        {
-            double d = (entityplayer.posY + 1.8200000000000001D) - (double)entityplayer.yOffset;
-
-            if (d - (double)j > 2D)
-            {
-                return 1;
-            }
-
-            if ((double)j - d > 0.0D)
-            {
-                return 0;
-            }
-        }
-
-        int l = MathHelper.floor_double((double)((entityplayer.rotationYaw * 4F) / 360F) + 0.5D) & 3;
-
-        if (l == 0)
-        {
-            return 2;
-        }
-
-        if (l == 1)
-        {
-            return 5;
-        }
-
-        if (l == 2)
-        {
-            return 3;
-        }
-        else
-        {
-            return l == 3 ? 4 : 0;
-        }
-    }
     
     @Override
     public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
     {
  
-            TileEntityFan var10 = (TileEntityFan)par1World.getBlockTileEntity(par2, par3, par4);
+           /* TileEntityFan var10 = (TileEntityFan)par1World.getBlockTileEntity(par2, par3, par4);
 
             if (var10 != null)
             {
-                //if(par5EntityPlayer.getCurrentEquippedItem().itemID == TrapcraftMod.upgradeRange.itemID)
-                //{
-                //	var10.extraRange = var10.extraRange + 0.5D;
-                //	par5EntityPlayer.getCurrentEquippedItem().stackSize--;
-                //	double totalRange = var10.extraRange + 5.0D;
-                //	if(!par1World.isRemote)
-                //	{
-                //		par5EntityPlayer.addChatMessage("Fan: Range is " + totalRange + " blocks.");
-                //	}
-                //    return true;
-                //}
-                //else if(par5EntityPlayer.getCurrentEquippedItem().itemID == TrapcraftMod.upgradeSpeed.itemID)
-                //{
-                //	var10.speed = var10.speed + 0.2F;
-                //	par5EntityPlayer.getCurrentEquippedItem().stackSize--;
-                //	double totalSpeed = var10.speed;
-                //	if(!par1World.isRemote)
-                //	{
-                //		par5EntityPlayer.addChatMessage("Fan: Speed is about " + totalSpeed + " blocks per second.");
-                //	}
-                //    return true;
-                //}
-            }
+                if(par5EntityPlayer.getCurrentEquippedItem().itemID == TrapcraftMod.upgradeRange.itemID)
+                {
+                	var10.extraRange = var10.extraRange + 0.5D;
+                	par5EntityPlayer.getCurrentEquippedItem().stackSize--;
+                	double totalRange = var10.extraRange + 5.0D;
+                	if(!par1World.isRemote)
+                	{
+                		par5EntityPlayer.addChatMessage("Fan: Range is " + totalRange + " blocks.");
+                	}
+                    return true;
+                }
+                else if(par5EntityPlayer.getCurrentEquippedItem().itemID == TrapcraftMod.upgradeSpeed.itemID)
+                {
+                	var10.speed = var10.speed + 0.2F;
+                	par5EntityPlayer.getCurrentEquippedItem().stackSize--;
+                	double totalSpeed = var10.speed;
+                	if(!par1World.isRemote)
+                	{
+                		par5EntityPlayer.addChatMessage("Fan: Speed is about " + totalSpeed + " blocks per second.");
+                	}
+                    return true;
+                }
+            }*/
 
             return false;
     }
