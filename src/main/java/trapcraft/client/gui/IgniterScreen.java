61f16ad74fab78d5dfb35b35761ceb6fce9073af
Alex Barter <alex.leo.barter@gmail.com>
2020-03-26 17:23:31 +0000

    Use RenderSystem for extra safety checks

--- src/main/java/trapcraft/client/gui/IgniterScreen.java
+++ src/main/java/trapcraft/client/gui/IgniterScreen.java
@@ -1,6 +1,6 @@
 package trapcraft.client.gui;
 
-import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
@@ -31,7 +31,7 @@ public class IgniterScreen extends ContainerScreen<IgniterContainer> {
 
     @Override
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
-        GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
         this.minecraft.getTextureManager().bindTexture(Constants.RES_GUI_IGNITER);
         int k = (this.width - this.xSize) / 2;
         int l = (this.height - this.ySize) / 2;
