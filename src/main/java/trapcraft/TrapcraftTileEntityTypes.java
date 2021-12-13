90e97ab50528dd19506f3c59de89d5d26a4fba53
Alex Barter <alex.leo.barter@gmail.com>
2021-12-13 20:12:36 +0000

    Bump Forge version

--- src/main/java/trapcraft/TrapcraftTileEntityTypes.java
+++ src/main/java/trapcraft/TrapcraftTileEntityTypes.java
@@ -1,7 +1,5 @@
 package trapcraft;
 
-import com.google.common.collect.Sets;
-
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,12 +10,14 @@ import trapcraft.block.tileentity.FanTileEntity;
 import trapcraft.block.tileentity.IgniterTileEntity;
 import trapcraft.block.tileentity.MagneticChestTileEntity;
 
+import java.util.Set;
+
 public class TrapcraftTileEntityTypes {
 
     public static final DeferredRegister<BlockEntityType<?>> TILE_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, Constants.MOD_ID);
 
-    public static final RegistryObject<BlockEntityType<MagneticChestTileEntity>> MAGNETIC_CHEST = TILE_ENTITIES.register("magnetic_chest", () -> new BlockEntityType<>(MagneticChestTileEntity::new, Sets.newHashSet(TrapcraftBlocks.MAGNETIC_CHEST.get()), null));
-    public static final RegistryObject<BlockEntityType<FanTileEntity>> FAN = TILE_ENTITIES.register("fan", () -> new BlockEntityType<>(FanTileEntity::new, Sets.newHashSet(TrapcraftBlocks.FAN.get()), null));
-    public static final RegistryObject<BlockEntityType<BearTrapTileEntity>> BEAR_TRAP = TILE_ENTITIES.register("bear_trap", () -> new BlockEntityType<>(BearTrapTileEntity::new, Sets.newHashSet(TrapcraftBlocks.BEAR_TRAP.get()), null));
-    public static final RegistryObject<BlockEntityType<IgniterTileEntity>> IGNITER = TILE_ENTITIES.register("igniter", () -> new BlockEntityType<>(IgniterTileEntity::new, Sets.newHashSet(TrapcraftBlocks.IGNITER.get()), null));
+    public static final RegistryObject<BlockEntityType<MagneticChestTileEntity>> MAGNETIC_CHEST = TILE_ENTITIES.register("magnetic_chest", () -> new BlockEntityType<>(MagneticChestTileEntity::new, Set.of(TrapcraftBlocks.MAGNETIC_CHEST.get()), null));
+    public static final RegistryObject<BlockEntityType<FanTileEntity>> FAN = TILE_ENTITIES.register("fan", () -> new BlockEntityType<>(FanTileEntity::new, Set.of(TrapcraftBlocks.FAN.get()), null));
+    public static final RegistryObject<BlockEntityType<BearTrapTileEntity>> BEAR_TRAP = TILE_ENTITIES.register("bear_trap", () -> new BlockEntityType<>(BearTrapTileEntity::new, Set.of(TrapcraftBlocks.BEAR_TRAP.get()), null));
+    public static final RegistryObject<BlockEntityType<IgniterTileEntity>> IGNITER = TILE_ENTITIES.register("igniter", () -> new BlockEntityType<>(IgniterTileEntity::new, Set.of(TrapcraftBlocks.IGNITER.get()), null));
 }
